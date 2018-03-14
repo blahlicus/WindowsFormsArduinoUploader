bbb18eabdaf1bdb4162228b068ebd6dc199150d5
blahlicus <blahlicus@gmail.com>
2018-03-15 02:24:36 +0800

    first commit

---
+++ WindowsFormsArduinoUploader/WindowsFormsArduinoUploader/FmMain.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsFormsArduinoUploader
+{
+    partial class FmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FmMain));
+            this.TBMain = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnBrowse = new System.Windows.Forms.Button();
+            this.CmbType = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CmbPort = new System.Windows.Forms.ComboBox();
+            this.BtnUpload = new System.Windows.Forms.Button();
+            this.PBMain = new System.Windows.Forms.ProgressBar();
+            this.RTBMain = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // TBMain
+            // 
+            resources.ApplyResources(this.TBMain, "TBMain");
+            this.TBMain.Name = "TBMain";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // BtnBrowse
+            // 
+            resources.ApplyResources(this.BtnBrowse, "BtnBrowse");
+            this.BtnBrowse.Name = "BtnBrowse";
+            this.BtnBrowse.UseVisualStyleBackColor = true;
+            this.BtnBrowse.Click += new System.EventHandler(this.BtnBrowse_Click);
+            // 
+            // CmbType
+            // 
+            resources.ApplyResources(this.CmbType, "CmbType");
+            this.CmbType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CmbType.FormattingEnabled = true;
+            this.CmbType.Name = "CmbType";
+            this.CmbType.SelectedIndexChanged += new System.EventHandler(this.CmbType_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // CmbPort
+            // 
+            resources.ApplyResources(this.CmbPort, "CmbPort");
+            this.CmbPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CmbPort.FormattingEnabled = true;
+            this.CmbPort.Name = "CmbPort";
+            // 
+            // BtnUpload
+            // 
+            resources.ApplyResources(this.BtnUpload, "BtnUpload");
+            this.BtnUpload.Name = "BtnUpload";
+            this.BtnUpload.UseVisualStyleBackColor = true;
+            this.BtnUpload.Click += new System.EventHandler(this.BtnUpload_Click);
+            // 
+            // PBMain
+            // 
+            resources.ApplyResources(this.PBMain, "PBMain");
+            this.PBMain.Name = "PBMain";
+            // 
+            // RTBMain
+            // 
+            resources.ApplyResources(this.RTBMain, "RTBMain");
+            this.RTBMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.RTBMain.Name = "RTBMain";
+            this.RTBMain.ReadOnly = true;
+            // 
+            // FmMain
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.RTBMain);
+            this.Controls.Add(this.PBMain);
+            this.Controls.Add(this.BtnUpload);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.CmbPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CmbType);
+            this.Controls.Add(this.BtnBrowse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TBMain);
+            this.Name = "FmMain";
+            this.Load += new System.EventHandler(this.FmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TBMain;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnBrowse;
+        private System.Windows.Forms.ComboBox CmbType;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox CmbPort;
+        private System.Windows.Forms.Button BtnUpload;
+        private System.Windows.Forms.ProgressBar PBMain;
+        private System.Windows.Forms.RichTextBox RTBMain;
+    }
+}
+
---
+++ WindowsFormsArduinoUploader/WindowsFormsArduinoUploader/FmMain.cs
@@ -0,0 +1,197 @@
+﻿using ArduinoUploader;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.IO.Ports;
+
+namespace WindowsFormsArduinoUploader
+{
+    public partial class FmMain : Form
+    {
+        string SelectedPort = "none";
+        string SelectedFile = "none";
+        List<string> portNums = new List<string>();
+        List<string> portNames = new List<string>();
+        BackgroundWorker bw;
+        ArduinoUploader.Hardware.ArduinoModel BoardType = 0;
+
+
+        private class SketchUploaderLogReceiver : IArduinoUploaderLogger
+        {
+            public void Error(string message, Exception exception)
+            {
+                Console.WriteLine("e: " + message);
+                
+            }
+
+            public void Warn(string message)
+            {
+                Console.WriteLine("w: " + message);
+                
+            }
+
+            public void Info(string message)
+            {
+                Console.WriteLine("i: " + message);
+
+            }
+
+            public void Debug(string message)
+            {
+                Console.WriteLine("d: " + message);
+
+                
+            }
+
+            public void Trace(string message)
+            {
+                Console.WriteLine(message);
+
+            }
+        }
+
+        public FmMain()
+        {
+            InitializeComponent();
+            bw = new BackgroundWorker();
+            bw.WorkerReportsProgress = true;
+
+            LoadPorts();
+            CmbType.DataSource = Enum.GetValues(typeof(ArduinoUploader.Hardware.ArduinoModel));
+            CmbPort.DataSource = portNames;
+            EventHook();
+        }
+
+
+        private void LoadPorts()
+        {
+            ManagementScope connectionScope = new ManagementScope();
+            SelectQuery serialQuery = new SelectQuery("SELECT * FROM Win32_SerialPort");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(connectionScope, serialQuery);
+
+            try
+            {
+                foreach (ManagementObject item in searcher.Get())
+                {
+                    string desc = item["Description"].ToString();
+                    string deviceId = item["DeviceID"].ToString();
+
+                    portNames.Add(desc + " (" + deviceId + ")");
+                    portNums.Add(deviceId);
+                }
+            }
+            catch (ManagementException e)
+            {
+                /* Do Nothing */
+            }
+            
+        }
+
+        private void EventHook()
+        {
+
+            bw.RunWorkerCompleted += (sender, e) => BWCompleted(sender, e);
+            bw.DoWork += (sender, e) => BWDoWork(sender, e);
+            bw.ProgressChanged += (sender, e) => BWReportProgress(sender, e);
+        }
+
+
+
+        private void BWCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            PBMain.Value = 0;
+            if (e.Error != null)
+            {
+                RTBMain.AppendText(e.Error.ToString());
+            }
+            else
+            {
+                RTBMain.AppendText("Successful upload to port " + SelectedPort + " with file " + SelectedFile);
+            }
+        }
+
+        private void BWDoWork(object sender, DoWorkEventArgs e)
+        {
+
+            var asuo = new ArduinoSketchUploaderOptions();
+            asuo.FileName = SelectedFile;
+            asuo.PortName = SelectedPort;
+            asuo.ArduinoModel = BoardType;
+            var logger = new SketchUploaderLogReceiver();
+
+            var progress = new Progress<double>(
+                p => logger.Info($"Programming progress: {p * 100:F1}% ..."));
+            progress.ProgressChanged += (isender, ie) => UploadProgressChanged(isender, ie);
+
+
+            var uploader = new ArduinoSketchUploader(asuo, logger, progress);
+
+            uploader.UploadSketch();
+        }
+
+        private void BWReportProgress(object sender, ProgressChangedEventArgs e)
+        {
+            PBMain.Value = e.ProgressPercentage;
+        }
+
+
+        private void UploadProgressChanged(object sender, double e)
+        {
+            int prog = Convert.ToInt32(e * 100);
+            bw.ReportProgress(prog);
+        }
+
+
+        private void FmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnBrowse_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Title = "Open Hex File";
+            ofd.Filter = "HEX Files | *.hex";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                TBMain.Text = ofd.FileName;
+            }
+        }
+
+        private void CmbType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Enum.TryParse<ArduinoUploader.Hardware.ArduinoModel>(CmbType.SelectedValue.ToString(), out BoardType);
+        }
+
+        private void BtnUpload_Click(object sender, EventArgs e)
+        {
+
+
+            if (File.Exists(TBMain.Text))
+            {
+                SelectedFile = TBMain.Text;
+                SelectedPort = portNums[CmbPort.SelectedIndex];
+                RTBMain.AppendText("Upload Started to port " + SelectedPort + " with file " + SelectedFile);
+                bw.RunWorkerAsync();
+            }
+        }
+    }
+}
